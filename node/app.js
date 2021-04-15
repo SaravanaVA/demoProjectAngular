b6e3d2cb85ac9c65598ac218999fb9590bb4fb0e
saravana_varathappanayyappan <saravana_varathappanayyappan@amat.com>
2021-04-15 08:48:48 +0100

    intial codes

---
+++ node/app.js
@@ -0,0 +1,16 @@
+import 'express';
+
+
+// body parser
+
+// define port
+let port = 3000;
+
+// routing will goes here
+
+res.listen('/save', ()=>{
+    // save to db logic goes here
+    // res.send()/
+});
+
+app.listenPort( port);
